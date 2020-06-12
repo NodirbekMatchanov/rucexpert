ae8f65dde451093451a921bd99410826db291da0
nodirbekintr <nodirbek@introvert.bz>
2020-06-12 15:56:08 +0500

    fix

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'supportEmail' => 'group.scala@mail.ru',
-//    'supportEmail' => 'support@ruc.expert',
+//    'supportEmail' => 'group.scala@mail.ru',
+    'supportEmail' => 'support@ruc.expert',
     'adminEmail' => 'group.scala@mail.ru',
     'sms_login' => 'rucexpert',
     'sms_passwd' => '9bq98rLNR',
