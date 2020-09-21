29db4d1eaf48831abdd9171b326eb5ad817f78ad
nodirbekintr <nodirbek@introvert.bz>
2020-09-21 07:12:49 +0500

    fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -33,13 +33,6 @@ NewAppAsset::register($this);
 <body class="loading-overlay-showing" data-plugin-page-transition data-loading-overlay
       data-plugin-options="{'hideDelay': 500}">
 <?php $this->beginBody() ?>
-<div class="loading-overlay">
-    <div class="bounce-loader">
-        <div class="bounce1"></div>
-        <div class="bounce2"></div>
-        <div class="bounce3"></div>
-    </div>
-</div>
 <div class="body">
 
     <?php
